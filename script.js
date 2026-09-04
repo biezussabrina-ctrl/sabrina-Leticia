8fb5d631ae5f890dbe61a8f8a5004ae08b6906d1
biezussabrina-ctrl <biezus.sabrina@escola.pr.gov.br>
2026-09-04 09:08:00 -0300

    Initialize script.js with question structure

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "perguntas1",
+        alternativas: [
+            "alternativa1", "alternativa2"
+        ]
+    },
+    {
+        enunciado: "perguntas1",
+        alternativas: [
+            "alternativa1", "alternativa2"
+        ]
+    }
+];
